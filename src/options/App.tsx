3707d2feb74dda01c55c1b4ab02370e370f69fde
jim wen <jim.wen@monash.edu>
2026-09-01 11:12:23 +1000

    refactor: rename Allowlist Page from Options

--- src/options/App.tsx
+++ src/options/App.tsx
@@ -1,20 +1,9 @@
-import { Allowlist } from "./pages/Allowlist";
 import { Default } from "./pages/Default";
 
-import { useAppContext } from "./context/context";
-
 export default function App() {
-  const {
-    optionsPage,
-  } = useAppContext();
-
   return (
     <main className="min-h-screen flex flex-col space-y-8 p-8">
-      {optionsPage === "allowlist" ? (
-        <Allowlist />
-      ) : (
-        <Default />
-      )}
+      <Default />
     </main>
   );
 }
